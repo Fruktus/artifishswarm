e984a1eddbdaf57cda799a93b564efa6d9a7b559
Fruktus <fruktusek@gmail.com>
2022-10-14 17:48:47 +0200

    implement random swimming

--- artifishswarm/afsa.py
+++ artifishswarm/afsa.py
@@ -1,30 +1,64 @@
+import random
+import numpy as np
+
+
 class AFSA:
-    def __init__(self, func, population: int, vision: float, crowding_factor: float, step: float):
+    """
+    Conventions:
+    x - coordinates, can be any dimension
+    y - the value that optimised function self.func takes for given x
+    """
+
+    def __init__(self, func, dimensions: int, population: int, max_iterations: int, vision: float, crowding_factor: float, step: float,
+                 search_retries: int, rand_seed=None):
         """
         Initialization of artificial fish swarm algorithm.
 
         :param func: The optimisation function
+        :param dimensions: the dimensionality of the function, dim(x) = dimensions-1, dim(y) = 1
         :param population: total number of artifish
+        :param max_iterations: how many iterations can be made at most while searching for optimum
         :param vision: the distance that the fish examines during optimisation
         :param crowding_factor: factor determining whether the area is overcrowded
             (compared with amount of fish/population)
         :param step: step length factor, used to calculate the distance of the fish movement
+        :param search_retries: how many times will the fish search for the food during search behavior
+        :param rand_seed: seed for the random number generator
         """
-        pass
+        random.seed(rand_seed)
+        np.random.seed(rand_seed)
 
-    def searching(self, retries: int):
+        self.func = func
+        self.dimensions = dimensions
+        self.population = population
+        self.max_iterations = max_iterations
+        self.vision = vision
+        self.crowding_factor = crowding_factor
+        self.step = step
+        self.search_retries = search_retries
+
+        self.fish = np.random.rand(self.population)
+        self.food = np.array([self.func(fish) for fish in self.fish])
+
+    def searching(self, fish_idx: int):
         """
         Searching Behavior - For fish in position Xi examine the surrounding area within vision.
         Assume that Yi, Yj are values of the optimised function for Xi, Xj.
         If the Yj > Yi, then move towards Xj.
 
-        :param retries: Defines how many times the fish will attempt searching for food before moving to default
-            'swimming' behavior
+        :param fish_idx: the index of fish which performs the search behavior
         :return: None
         """
-        pass
+        for _ in range(self.search_retries):
+            target_x = self.fish[fish_idx] + self.vision * random.random()
 
-    def swarming(self, asd):
+            if self.func(target_x) > self.func(fish_idx):
+                self.make_step(fish_idx, target_x)
+                return
+
+        self.swimming(fish_idx)
+
+    def swarming(self):
         """
         Swarming Behavior - the fish examines its surroundings for the area with other fishes and more food
         (higher Y value).
@@ -33,7 +67,6 @@ class AFSA:
         direction.
         Otherwise, do the search behavior.
 
-        :param asd:
         :return:
         """
         pass
@@ -59,10 +92,27 @@ class AFSA:
         """
         pass
 
-    def swimming(self):
+    def swimming(self, fish_idx: int):
         """
         Swimming Behavior - swim in the randomly chosen direction.
 
+        :param fish_idx: the index of fish which performs swimming behavior
         :return:
         """
-        pass
+
+        self.fish[fish_idx] += self.vision * random.random()
+
+    def make_step(self, fish_idx: int, destination_x: float):
+        """
+        Moves the fish towards the distance (in place), takes the modifiers into account.
+
+        :param fish_idx: index of the fish to move
+        :param destination_x: the destination x position
+        :return:
+        """
+        current_x = self.fish[fish_idx]
+
+        new_x = self.fish[fish_idx] + ((destination_x - current_x)/np.linalg.norm(destination_x - current_x)) \
+                * self.step * random.random()  # TODO bottom part should be the vector length
+
+        self.fish[fish_idx] = new_x
