f9c35b78663c9ef6166de54934b9a7084f27b799
Kamil Jarosz <kjarosh256@gmail.com>
2022-11-22 14:11:41 +0100

    Improve basic example and add crowding factor example

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -11,10 +11,13 @@ def func(x):  # sample function to test the fishies behavior
 
 
 def main():
+    show_before = False
     afsa = AFSA(func=func,
                 dimensions=1,  # our function takes one value and returns one value
                 population=50,  # other parameters chosen empirically
                 max_iterations=20,
+                low=-2,
+                high=3,
                 vision=0.5,
                 crowding_factor=0.98,
                 step=0.5,
@@ -22,17 +25,21 @@ def main():
                 rand_seed=12345  # the seed makes the results reproducible
                 )
 
-    afsa.run()
-    fish_array = afsa.fish
+    if show_before:
+        plot(afsa.fish)
 
-    x_data = np.arange(-2.0, 3.0, 0.1).reshape((-1, 1))
+    afsa.run()
+    plot(afsa.fish)
+
+
+def plot(fish_array):
+    x_data = np.arange(-2.5, 3.5, 0.1).reshape((-1, 1))
     fig1 = px.line(
         x=x_data.reshape(-1),
         y=[func(x) for x in x_data],
     )
     fig2 = px.scatter(x=fish_array.reshape(-1), y=[func(x) for x in fish_array])
     fig2.update_traces(marker=dict(color='red'))
-
     fig3 = go.Figure(data=fig1.data + fig2.data)
     fig3.show()
 
